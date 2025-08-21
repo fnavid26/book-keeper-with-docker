db5f010ffdc158c300536b3cc0b9f8b0f7586368
Fateen Navid <fateen.navid@brainstation-23.com>
2025-08-21 13:15:05 +0600

    added seperate setup for dev and prod

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_USER = os.getenv("POSTGRES_USER", "postgres")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
